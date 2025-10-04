d4bdb5943e2277ae9d532946d61b3ba63daa7728
PatriksM <pmorevs@gmail.com>
2025-10-04 10:56:33 +0300

    ediena koordinates

--- Patriks/js/script.js
+++ Patriks/js/script.js
@@ -4,8 +4,17 @@ const ctx = canvas.getContext("2d");
 const sp_laukums = new Image();
 sp_laukums.src = "img/ground.png";
 
+const sp_mango = new Image();
+sp_mango.src = "img/mango.png";
+
+let ediens = {
+    x: 400,
+    y: 300
+}
+
 function zimetSpeli() {
     ctx.drawImage(sp_laukums, 0, 0);
+    ctx.drawImage(sp_mango, ediens.x, ediens.y);
 }
 
 let spele = setInterval(zimetSpeli, 100);
