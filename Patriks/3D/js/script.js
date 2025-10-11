a43a08b97b18a6ad4c4b8ec89c9d0bf2ab79f73f
PatriksM <pmorevs@gmail.com>
2025-10-11 11:01:15 +0300

    jauns klucis ar ciklu

--- Patriks/3D/js/script.js
+++ Patriks/3D/js/script.js
@@ -1,5 +1,10 @@
 var map = [
-    [0, 0, 0, 0, 0, 0, 200, 200, "red", 0.5] //x, y, z, rx, ry, rz, width, height, color, opacity
+    [0, 0, 0, 0, 0, 0, 200, 200, "red", 0.5], //x, y, z, rx, ry, rz, width, height, color, opacity
+    [0, 0, -200, 0, 0, 0, 200, 200, "black", 0.5],
+    [-100, 0, -100, 0, 90, 0, 200, 200, "orange", 0.5],
+    [100, 0, -100, 0, 90, 0, 200, 200, "yellow", 0.5],
+    [0, 100, -100, 90, 0, 0, 200, 200, "blue", 0.5],
+    [0, -100, -100, 90, 0, 0, 200, 200, "violet", 0.5]
 ];
 
 var world = document.getElementById("world");
@@ -17,6 +22,7 @@ function createWorld() { // 3D pasaules izveide
         jaunaSiena.style.height = `${map[i][7]}px`;
         jaunaSiena.style.backgroundColor = map[i][8];
         jaunaSiena.style.opacity = map[i][9];
+        jaunaSiena.style.transform = `translate3d(${600 + map[i][0] - map[i][6]/2}px, ${400 + map[i][1] - map[i][7]/2}px, ${map[i][2]}px) rotateX(${map[i][3]}deg) rotateY(${map[i][4]}deg) rotateZ(${map[i][5]}deg)`;
         world.append(jaunaSiena);
     }
 }
