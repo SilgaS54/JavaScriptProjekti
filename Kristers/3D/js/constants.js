f23026aa180fa9f81d1af52d6801af398c4d777c
Kristers Šlakorcins <kristers.slakorcins@azeron.eu>
2025-11-01 10:44:03 +0200

    mapes

---
+++ Kristers/3D/js/constants.js
@@ -0,0 +1,7 @@
+//2pi RAD IR 360 DEG
+// 1 DEG radiānos ir 2PI/360 jeb PI/180
+var deg = Math.PI/180;
+
+var level = 0;
+var g = 0.1;
+var onGround = false;
---
+++ Kristers/3D/js/teleporti.js
@@ -0,0 +1,15 @@
+var teleports= [
+  [-800, 0, -800, 0, 45, 0, 200, 200, "yellow", 0.3],
+]; // 1. līmenis
+
+//Silgas kods
+var teleportsSilga= [
+  [-900, 0, -900, 0, 45, 0, 200, 200, "yellow", 0.3],
+];
+//2. līmenis
+
+//Kristera teleports
+var teleportsKristers= [
+  [-800, 0, 800, 0, 45, 0, 200, 200, "yellow", 0.3],
+];
+//3. līmenis
