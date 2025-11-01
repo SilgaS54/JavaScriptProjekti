4a6ce3097183a8804057e3023f750b2b0132c4ee
Kristers Šlakorcins <kristers.slakorcins@azeron.eu>
2025-11-01 11:51:55 +0200

    menu

--- Kristers/3D/js/elementi.js
+++ Kristers/3D/js/elementi.js
@@ -3,3 +3,6 @@ var mansTeksts = document.getElementById("mansTeksts");
 var myH1 = document.createElement("h1");
 
 var world = document.getElementById("world");
+
+var menuStart = document.getElementById("menuStart");
+var buttonStart = document.getElementById("buttonStart");
--- Kristers/3D/js/eventListeners.js
+++ Kristers/3D/js/eventListeners.js
@@ -6,27 +6,29 @@ var pressUp = 0;
 var mouseX = 0;
 var mouseY = 0;
 
-document.addEventListener("keydown", (event) => { //reģistrējam taustiņu nospiešanu
-    if(event.key == "w") pressForward = pawn.vz;
-    if(event.key == "s") pressBack = pawn.vz;
-    if(event.key == "a") pressLeft = pawn.vx;
-    if(event.key == "d") pressRight = pawn.vx;
-    if(event.keyCode == 32) pressUp = pawn.vy;
-    soluSkana.play();
-})
+document.addEventListener("keydown", (event) => {
+  //reģistrējam taustiņu nospiešanu
+  if (event.key == "w") pressForward = pawn.vz;
+  if (event.key == "s") pressBack = pawn.vz;
+  if (event.key == "a") pressLeft = pawn.vx;
+  if (event.key == "d") pressRight = pawn.vx;
+  if (event.keyCode == 32) pressUp = pawn.vy;
+  // soluSkana.play();
+});
 
-document.addEventListener("keyup", (event) => { //reģistrējam taustiņu atspiešanu
-    if(event.key == "w") pressForward = 0;
-    if(event.key == "s") pressBack = 0;
-    if(event.key == "a") pressLeft = 0;
-    if(event.key == "d") pressRight = 0;
-    if(event.keyCode == 32) pressUp = 0;
-    soluSkana.pause();
-})
+document.addEventListener("keyup", (event) => {
+  //reģistrējam taustiņu atspiešanu
+  if (event.key == "w") pressForward = 0;
+  if (event.key == "s") pressBack = 0;
+  if (event.key == "a") pressLeft = 0;
+  if (event.key == "d") pressRight = 0;
+  if (event.keyCode == 32) pressUp = 0;
+  // soluSkana.pause();
+});
 
 //pēles kustības apstrāde
 
 document.addEventListener("mousemove", (event) => {
-    mouseX = event.movementX;
-    mouseY = event.movementY;
-})
+  mouseX = event.movementX;
+  mouseY = event.movementY;
+});
