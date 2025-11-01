6bff2cb19a0fde51c4fb771f6137885f2ea120d0
PatriksM <pmorevs@gmail.com>
2025-11-01 10:22:13 +0200

    Vairaki faili JS

---
+++ Patriks/3D/js/player.js
@@ -0,0 +1,13 @@
+class player {
+    constructor(x, y, z, rx, ry) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.rx = rx;
+        this.ry = ry;
+        this.vx = 5;//ātrums x virzienā
+        this.vy = 5;//ātrums y (vertikālā virzienā)
+        this.vz = 5;//ātrums z virzienā
+    }
+} 
+
