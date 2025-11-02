901695b27b48d227bfe00384015e3871511e9ea8
Kristers Šlakorcins <kristers.slakorcins@azeron.eu>
2025-11-02 09:46:02 +0200

    level 2

--- Kristers/3D/js/mainscript.js
+++ Kristers/3D/js/mainscript.js
@@ -5,15 +5,24 @@ var spelesElementi = [
   [mapKristers, objektiKristers, teleportsKristers],
 ];
 
-buttonStart.onclick = function () {
-  menuStart.style.display = "none";
-  canlock = true;
-};
-
 var punkti = 0;
 var pawn = new player(90, 0, 90, 0, 0);
 
-createWorld(spelesElementi[level][0]);
-zimetObjektus(spelesElementi[level][1]);
-zimetObjektus(spelesElementi[level][2]);
-timerGame = setInterval(update, 10);
+buttonStart.onclick = function () {
+  menuStart.style.display = "none";
+  canlock = true;
+  createWorld(spelesElementi[level][0]);
+  zimetObjektus(spelesElementi[level][1]);
+  zimetObjektus(spelesElementi[level][2]);
+  timerGame = setInterval(update, 10);
+};
+
+buttonLimenis.onclick = function () {
+  menuStart.style.display = "none";
+  canlock = true;
+  level = 1;
+  createWorld(spelesElementi[level][0]);
+  zimetObjektus(spelesElementi[level][1]);
+  zimetObjektus(spelesElementi[level][2]);
+  timerGame = setInterval(update, 10);
+};
