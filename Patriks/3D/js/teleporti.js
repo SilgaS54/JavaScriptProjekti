a3d3ec59d1f1842bdf5f882aa635f9993ccb81fd
PatriksM <pmorevs@gmail.com>
2025-11-02 11:12:21 +0200

    teleportu attēlu un objektu attēlu iespējas

--- Patriks/3D/js/teleporti.js
+++ Patriks/3D/js/teleporti.js
@@ -1,5 +1,19 @@
+//-----------------
+// 0. - x koordināte
+// 1. - y koordināte
+// 2. - z koordināte
+// 3. - pagieziena leņķis pa OX asi 
+// 4. - pagieziena leņķis pa OY asi 
+// 5. - pagieziena leņķis pa OZ asi
+// 6. - platums
+// 7. - augstums
+// 8. - krāsa
+// 9. - caursīdīgums [0 .. 1] 
+// 10. - tekstūra
+//-----------------
+
 var teleports= [
-  [-800, 0, -800, 0, 45, 0, 200, 200, "yellow", 0.3],
+  [-800, 0, -800, 0, 45, 0, 200, 200, "yellow", 1, "url(textures/teleport.gif)"],
 ]; // 1. līmenis
 
 //Silgas kods
--- Patriks/3D/js/zimetObjektus.js
+++ Patriks/3D/js/zimetObjektus.js
@@ -9,7 +9,12 @@ function zimetObjektus(mansObj, name){
         // jaunsObjekts.style.position = "absolute";
         jaunsObjekts.style.width = `${mansObj[i][6]}px`;
         jaunsObjekts.style.height = `${mansObj[i][7]}px`;
-        jaunsObjekts.style.backgroundColor = mansObj[i][8];
+        if(mansObj[i][10]){
+            jaunsObjekts.style.backgroundSize = "100% 100%";
+            jaunsObjekts.style.backgroundImage = mansObj[i][10];
+        } else {
+            jaunsObjekts.style.backgroundColor = mansObj[i][8];
+        }
         jaunsObjekts.style.opacity = mansObj[i][9];
         jaunsObjekts.style.transform = `translate3d(${600 + mansObj[i][0] - mansObj[i][6]/2}px, ${400 + mansObj[i][1] - mansObj[i][7]/2}px, ${mansObj[i][2]}px) rotateX(${mansObj[i][3]}deg) rotateY(${mansObj[i][4]}deg) rotateZ(${mansObj[i][5]}deg)`;
         world.append(jaunsObjekts);
