a4def06eb571eb64bdba8bd1f0889aaa92fc1ec6
Silga <silga.stankuna@gmail.com>
2025-11-02 11:43:05 +0200

    monētas

--- Silga/3D/js/teleporti.js
+++ Silga/3D/js/teleporti.js
@@ -1,10 +1,24 @@
+//-----------------
+// 0. - x koordināte
+// 1. - y koordināte
+// 2. - z koordināte
+// 3. - pagieziena leņķis pa OX asi 
+// 4. - pagieziena leņķis pa OY asi 
+// 5. - pagieziena leņķis pa OZ asi
+// 6. - platums
+// 7. - augstums
+// 8. - krāsa
+// 9. - caursīdīgums [0 .. 1] 
+// 10. - tekstūra
+//-----------------
+
 var teleports= [
-  [-800, 0, -800, 0, 45, 0, 200, 200, "yellow", 0.3],
+  [-800, 0, -800, 0, 45, 0, 200, 200, "yellow", 1, "url(textures/teleport.gif)"],
 ]; // 1. līmenis
 
 //Silgas kods
 var teleportsSilga= [
-  [-900, 0, -900, 0, 45, 0, 200, 200, "yellow", 0.3],
+  [-900, 0, -900, 0, 45, 0, 200, 200, "yellow", 1, "url(textures/teleport2.gif)"],
 ];
 //2. līmenis
 
