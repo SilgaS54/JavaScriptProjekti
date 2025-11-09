dd0d490bb66fddbbc848b8f09e747ecfa46dd122
Kristers Šlakorcins <kristers.slakorcins@azeron.eu>
2025-11-09 09:31:53 +0200

    šaušana

--- Kristers/3D/js/interact.js
+++ Kristers/3D/js/interact.js
@@ -18,13 +18,32 @@ function interact(obj) {
       obj[i][1] = 100000;
       obj[i][2] = 100000;
     }
-    if (i == 0) {
-      // console.log(`${obj[i]}`);
-      var mobj = document.getElementById("objekts0");
-      var rect = mobj.getBoundingClientRect();
-      //console.log(`${rect.x} ${rect.y} ${rect.z} ${rect.width} ${rect.height}`);
-      // console.log(`${coorReTransform(obj[i][0], obj[i][1], obj[i][2], pawn.rx, pawn.ry, pawn.rz)}`);
-      // console.log(`${coorTransform(obj[i][0], obj[i][1], obj[i][2], pawn.rx, pawn.ry, pawn.rz)}`);
-    }
   }
+
+  document.onclick = function () {
+    for (let j = 0; j < obj.length; j++) {
+      var mobj = document.getElementById(`objekts${j}`);
+      var rect = mobj.getBoundingClientRect();
+      if (
+        Math.abs(
+          Math.sqrt((600 - rect.x) ** 2 + (400 - rect.y) ** 2) -
+            Math.sqrt(rect.width ** 2 / 4 + rect.height ** 2 / 4)
+        ) < 20
+      ) {
+        panemsanasSkana.play();
+        punkti++;
+        myH1.textContent = `Punkti: ${punkti} no ${obj.length}`;
+        if (punkti == obj.length) {
+          var myH2 = document.createElement("h1");
+          myH2.textContent = "Punkti sakrāti, var doties uz teleportu!";
+          mansTeksts.appendChild(myH2);
+        }
+        mansTeksts.appendChild(myH1);
+        console.log("object HIT!");
+        obj[j][0] = 100000;
+        obj[j][1] = 100000;
+        obj[j][2] = 100000;
+      }
+    }
+  };
 }
