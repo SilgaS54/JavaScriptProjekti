1f71e1a5c9481ba91abb92bd1c4e2214d7e868e6
Silga <silga.stankuna@gmail.com>
2025-12-07 10:16:35 +0200

    Kopā

--- Silga/3D/js/interact.js
+++ Silga/3D/js/interact.js
@@ -3,7 +3,7 @@ function interact(obj) {
     let r = (pawn.x - obj[i][0]) ** 2 + (pawn.y - obj[i][1]) ** 2 + (pawn.z - obj[i][2]) ** 2;
     if (r < (obj[i][6]) ** 2 + (obj[i][7]) ** 2) {
       panemsanasSkana.play();
-      punkti++;
+      punkti += 1; // Viens punkts
       myH1.textContent = `Punkti: ${punkti} no ${obj.length}`;
       if (punkti == obj.length) {
         var myH2 = document.createElement("h1");
@@ -17,26 +17,26 @@ function interact(obj) {
     }
   }
 
-    // Šaušanas iespēja pēc koordinātēm
-    // for (let j = 0; j < obj.length; j++) {
-    //   var mobj = document.getElementById(`objekts${j}`);
-    //   var rect = mobj.getBoundingClientRect();
-    //   if (Math.abs((Math.sqrt((600 - rect.x) ** 2 + (400 - rect.y) ** 2)) - (Math.sqrt(rect.width ** 2 / 4 + rect.height ** 2 / 4))) < 20) {
-    //     panemsanasSkana.play();
-    //     punkti++;
-    //     myH1.textContent = `Punkti: ${punkti} no ${obj.length}`;
-    //     if (punkti == obj.length) {
-    //       var myH2 = document.createElement("h1");
-    //       myH2.textContent = "Punkti sakrāti, var doties uz teleportu!"
-    //       mansTeksts.appendChild(myH2);
-    //     }
-    //     mansTeksts.appendChild(myH1);
-    //     console.log("object HIT!");
-    //     obj[j][0] = 100000;
-    //     obj[j][1] = 100000;
-    //     obj[j][2] = 100000;
-    //   }
-    // } -------------------BEIGAS
+  // Šaušanas iespēja pēc koordinātēm
+  // for (let j = 0; j < obj.length; j++) {
+  //   var mobj = document.getElementById(`objekts${j}`);
+  //   var rect = mobj.getBoundingClientRect();
+  //   if (Math.abs((Math.sqrt((600 - rect.x) ** 2 + (400 - rect.y) ** 2)) - (Math.sqrt(rect.width ** 2 / 4 + rect.height ** 2 / 4))) < 20) {
+  //     panemsanasSkana.play();
+  //     punkti++;
+  //     myH1.textContent = `Punkti: ${punkti} no ${obj.length}`;
+  //     if (punkti == obj.length) {
+  //       var myH2 = document.createElement("h1");
+  //       myH2.textContent = "Punkti sakrāti, var doties uz teleportu!"
+  //       mansTeksts.appendChild(myH2);
+  //     }
+  //     mansTeksts.appendChild(myH1);
+  //     console.log("object HIT!");
+  //     obj[j][0] = 100000;
+  //     obj[j][1] = 100000;
+  //     obj[j][2] = 100000;
+  //   }
+  // } -------------------BEIGAS
 
   // } 29.11
 }
